310fdfe624902cc1f39cc57c87c44c273f70bd0a
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2018-04-19 00:47:57 +0100

    Removed isValid (js and css) to it's own repo Updated Readme Updated documentation in general

--- README.md
+++ README.md
@@ -1,238 +1,96 @@
 # Current avaiable functions
-  - [isValid](#isvalid)
   - [isEmpty](#isempty)
-  - [isPositve](#ispositive)
+  - [isPositive](#ispositive)
   - [isEven](#iseven)
   - [isArray](#isarray)
   - [isObject](#isobject)
   - [isDOM](#isdom)
+  - [isString](#isstring)
+  - [isFunction](#isfunction)
+  - [isNumber](#isnumber)
+  - [escapeString](#escapestring)
   - [Array.pushUnique](#pushunique)
   - [Array.removeIfExists](#removeifexists)
   - [Date.formatDate](#formatdate)
 
 # Instalation
-Simply download and include the script to your file: ``<script src="path/to/script"></script>``
-If you want this lib to also handle validation errors (correctly) on isValid function, you must also download and include de css file ``<link rel="stylesheet" href="css/style.css">``
+You can either download and include the script to your file: ``<script src="path/to/script"></script>`` manually or you can use ecmascript 6's ``require("utilities-js")`` 
 
 # Usage
-Independetly of which enviorenment you're working on, all you need to do is call the object util.function, for example, `` util.isEmpty(value)`` 
-
-## isValid
-This function validates DOM fields or simple values. Some of the rules also require a rule value, for instance when you using maxvalue you have to specify what's the max value. Every rule has it's own error message, however you can send your own custom error message (showed below).
-This function accepts either and object or an array of objects. This object contains the field / value to be validated, the rule, a custom message (optional), ruleValue (depends on the which rule we're applying) and optional (which is in itself optional)
-
-Object composition: 
-```sh
-{
-  field: DOM element or value,
-  rule: validation rule (string),
-  message: a custom message you want for validation errors (string),
-  ruleValue: some rules require a rulevalue (see below) 
-  optional: for the times when a value is optional but must meet certain criteria (boolean)
-}
-```
-    
-
-Currently supports the following rules:
-  - [required](#required)
-  - [number](#number)
-  - [even](#even)
-  - [maxvalue](#maxvalue)
-  - [minvalue](#minvalue)
-  - [positive](#positive)
-  - [maxlen](#maxlen)
-  - [minlen](#minlen)
-  - [email](#email)
-  - [equal](#equal)
-  - [phone](#phone)
-
-### Examples
-There are several ways for you to validate what you need. You can:
-   - validate only one field with one validation rule
-   - validate only one field with multiple validation rule
-   - validate multiple fields with one validation rule each
-   - validate multiple fields with multiple validation rule each
-   - validate multiple fields with one validation rule for some fields and multiple for others
-You can also have a specific custom message for each validation rule
-
-#### One field, one rule
-```sh
-util.isValid({
-  input: value,
-  rule: "required"
-});
-```
-
-#### One field, multiple rules
-```sh
-util.isValid({
-  input: value, 
-  rule: ["required", "number"]
-});
-```
-
-#### Multiple fields, one rule each
-```sh
-util.isValid([
-  {input: value, rule: "required"},
-  {input: value2, rule: "email"},
-  {input: value3, rule: "even"}
-]);
-```
-
-#### Multiple fields, multiple rules
-```sh
-util.isValid([
-  {input: value, rule: ["required", "even"]},
-  {input: value2, rule: ["email", "positive"]}
-]);
-```
-
-#### Multiple fields, mixed rules
-```sh
-util.isValid([
-  {input: value, rule: "required"},
-  {input: value2, rule: ["email", "positive"]}
-]);
-```
-
-#### Multiple rules with custom message each
-```sh
-util.isValid([
-  {input: value, rule: "required"},
-  {input: value2, rule: [
-    {rule: "email", message: "Custom message"},
-    {rule: "positive", message: "Custom message 2"} 
-  ]}
-]);
-```
-
-#### Rules with rule value
-```sh
-util.isValid([
-  {input: value, rule: "maxvalue", ruleValue: 5},
-  {input: value2, rule: "required", message: "something"},
-  {input: value3, rule: "maxvalue", ruleValue: 5, message: "another something"},
-  {input: value4, rule: [
-    {rule: "minlen", message: "Custom message", ruleValue: 5},
-    {rule: "positive", message: "Custom message 2"},
-    {rule: "required"}
-  ]}
-]);
-```
-
-### required
-Checks if the value has something
-```sh
-util.isValid({input: value, rule: "required"});
-```
-
-### number
-Checks if the value is a number
-```sh
-util.isValid({input: value, rule: "number"});
-```
-
-### even
-Checks if the value is a number and if it's an even number
-```sh
-util.isValid({input: value, rule: "even"});
-```
-
-### maxvalue
-Checks if the value is lower than the given rule value
-```sh
-util.isValid({input: value, rule: "maxvalue", ruleValue: 5});
-```
-
-### minvalue
-Checks if the value is higher than the given rule value
-```sh
-util.isValid({input: value, rule: "minvalue", ruleValue: 5});
-```
-
-### positive
-Checks if the value is a number and if it's positive
-```sh
-util.isValid({input: value, rule: "maxvalue", ruleValue: 5});
-```
-
-### equal 
-Checks if the value equals the value(s) from the rule value
-```sh
-util.isValid({input: value, rule: "equal", ruleValue: 5});
-```
-or 
-```sh
-util.isValid({input: value, rule: "equal", ruleValue: [5, 2, 3]});
-```
-
-### maxlen
-Checks if the value length is lower than the given rule value
-```sh
-util.isValid({input: value, rule: "maxlen", ruleValue: 5});
-```
-
-### minlen
-Checks if the value length is higher than the given rule value
-```sh
-util.isValid({input: value, rule: "minlen", ruleValue: 5});
-```
-
-### email
-Checks if the value is a valid email
-```sh
-util.isValid({input: value, rule: "email"});
-```
-
-### phone
-Checks if the value is a valid phone number, according to the cirteria set by the rule value
-```sh
-util.isValid({input: value, rule: "phone", ruleValue: ["### ### ###", "#########"]});
-```
-or 
-```sh
-util.isValid({input: value, rule: "phone", ruleValue: "#########"});
-```
+Depending on your development environment you may need to call `` util. `` or use the functions directly. For instance if you're on browser and node you need to do `` util.isEmpty(value) `` but on react native you can import each function seperately:  `` import { isEmpty } from "utilities-js"; `` and then `` isEmpty(value) `` .
 
 ## isEmpty
-Pretty self explanatory, checks if the value is empty. Returns true if it is and false otherwise
+Pretty self explanatory, checks if the value is empty.
+Returns true if it is and false otherwise.
 ```sh
 util.isEmpty(value) 
 ```
 
 ## isPositive
-Checks if the value is numeric and positive. Returns true if it is and false otherwise
+Checks if the value is numeric and positive.
+Throws an Error if the value isn't a number.
+Returns true if it is and false otherwise.
 ```sh
 util.isPositive(value) 
 ```
 
 ## isEven
-Checks if the value is numeric and even. Returns true if it is and false otherwise
+Checks if the value is numeric and even.
+Throws an Error if the value isn't a number
+Returns true if it is and false otherwise.
 ```sh
 util.isEven(value) 
 ```
 
 ## isArray
-Checks if the value is an array. Returns true if it is and false otherwise
+Checks if the value is an array.
+Returns true if it is and false otherwise.
 ```sh
 util.isArray(value) 
 ```
 
 ## isDOM
-Checks if the value is an HTML DOM object. Returns true if it is and false otherwise
+Checks if the value is an HTML DOM object.
+Returns true if it is and false otherwise.
 ```sh
 util.isDOM(value)
 ```
 
 ## isObject
-Checks if the value is an object. Returns true if it is and false otherwise
+Checks if the value is an object.
+Throws an Error if the value is empty.
+Returns true if it is and false otherwise.
+```sh
+util.isObject(value) 
+```
+
+## isFunction
+Checks if the value is an function.
+Throws an Error if the value is empty.
+Returns true if it is and false otherwise.
+```sh
+util.isFunction(value) 
+```
+
+## isNumber
+Checks if the value is an number.
+Returns true if it is and false otherwise.
+```sh
+util.isNumber(value) 
+```
+
+## escapeString
+Sanitizes a string, escaping special characters, double and single quotation marks and removing white spaces at the start and end of the string.
+Throws an Error if the value is not a string.
+Returns escaped string.
 ```sh
 util.isObject(value) 
 ```
 
 ## pushUnique
-This is a Array.prototype function meaning it's only accessable through a instantiated array. This function checks if the value exists on the array and if it doesn't it'll push it in.
+This is a Array.prototype function meaning it's only accessable through a instantiated array. 
+This function checks if the value exists on the array and if it doesn't it'll push it in.
+Returns false if the value is in the array and true otherwise.
 ```sh
 var arr = [1, 2, 3];
 arr.pushUnique(2); // does nothing
@@ -241,6 +99,7 @@ arr.pushUnique(4); // adds 4 to the array
 
 ## removeIfExists
 Also a prototype function. This one checks if an element exists in the array and if so, removes it.
+Returns nothing.
 ```sh
 var arr = [1, 2, 3];
 arr.removeIfExists(2); // removes 2
@@ -248,7 +107,9 @@ arr.removeIfExists(4); // does nothing
 ```
 
 ## formatDate 
-Can be used either on an instantiated object or not. Recieves a date (either string or Date) and returns it in EU, US or MySQL-ready format based on the type.
+Can be used either on an instantiated object or not. Formats a date to either EU, US or MySQL-ready.
+Returns the formated date
+Throws an Error if the date is empty or if the date was invalid and couldn't create a date instance with the received date.
 ```sh
 Date.formatDate(new Date(), type); 
 ```
--- libs/utilities.js
+++ libs/utilities.js
@@ -1,328 +1,13 @@
 /**
  * @author Tiago Ribeiro - www.tiago-ribeiro.com
- * @description Helper class that has several utilitary functions such as field validation, isEmpty, isArray, etc
+ * @description Helper class that has several utilitary functions such as isEmpty, isArray, formatDate, etc
  * @see https://github.com/Ribeiro-Tiago/util
- * @copyright ISC license, 2017
- * @version 1.0.8
+ * @copyright MIT license, 2017
+ * @version 1.1.0
  */
 
-/**
- * Helper class that has several utilitary functions such as field validation, isEmpty, isArray, etc
- */
 (function(){
-    'use strict';
-
-    /**
-     * Validates the recieved obj according the validation rules recieved
-     * @param {array|object} obj - array or single object with obj (either DOM object or value) to be validated, validation rules and custom error messages (if the user wants)
-     * example of what an object would look like: {'input': '}
-     * @param {boolean} errorHandle - tells us if we're handling validation errors or not
-     * @throws {Error} - throws this exception if arguments are empty
-     * @return {boolean} - returns true if every input is valid and false if not
-     */
-    const isValid = function(obj, errorHandle){
-        let handleErrors = ((typeof module !== 'undefined' && typeof module.exports !== 'undefined') || (typeof define === 'function' && define.amd)) ? false : (errorHandle || true);
-        
-        if (isEmpty(obj))
-            throw new Error("Invalid arguments!");
-
-        let errors = [];
-
-        /**
-         * Checks the validation rules for a certain input and validates it. If need be, adds new entry to errors array
-         * @param {string} input - input to validate. Either DOM element or simple value
-         * @param {string} rule - validation rule
-         * @param {string} message - custom error message
-         * @param {number} ruleValue - if the validation needs a value (e.g.: maxvalue, minvalue), this is that value
-         * @throws {Error} - throws exception if rule is valid or one of the ruleValues isn't valid
-         */
-        const validate = function(input, rule, message, ruleValue, optional){
-            let inputValue = (input.tagName) ? input.value : input[0].value;
-            
-            if (optional && isEmpty(inputValue))
-                return true;
-
-            if (rule === "required")
-            {
-                if (isEmpty(inputValue))
-                {
-                    errors.push({
-                        error: message || "Required field!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "number")
-            {
-                if (!isNumber(inputValue))
-                {
-                    errors.push({
-                        error: message || "Numeric field!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "even")
-            {
-                if (!isEven(inputValue))
-                {
-                    errors.push({
-                        error: message || "Value must be even!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "maxvalue")
-            {
-                if (!isNumber(ruleValue))
-                    throw new Error ("Error validating maxvalue: value isn't number!");
-
-                if (parseInt(inputValue) > parseInt(ruleValue))
-                {
-                    errors.push({
-                        error: message || "Value must be below " + ruleValue + "!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "minvalue")
-            {
-                if (!isNumber(ruleValue))
-                    throw new Error ("Error validating maxvalue: value isn't number!");
-                
-                if (parseInt(inputValue) < parseInt(ruleValue))
-                {
-                    errors.push({
-                        error: message || "Value must be above " + ruleValue + "!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "positive")
-            {
-                if (!isPositive(inputValue))
-                {
-                    errors.push({
-                        error: message || "Field must be positive!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "equal")
-            {
-                let incorrectValue = function(value){
-                    return (parseInt(inputValue) !== parseInt(value));
-                }
-
-                let different = (isObject(ruleValue)) ? ruleValue.every(incorrectValue) : incorrectValue(ruleValue);
-                
-                if (different)
-                {
-                    errors.push({
-                        error: message || "Value must be one of the following: " + ruleValue + "!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "maxlen")
-            {
-                if (inputValue.length > ruleValue)
-                {
-                    errors.push({
-                        error: message || "Maximum value length: " + ruleValue + "!",
-                        input: input
-                    });
-                }
-            }
-            
-            else if (rule === "minlen")
-            {
-                if (inputValue.length < ruleValue)
-                {
-                    errors.push({
-                        error: message || "Minimum value length: " + ruleValue + "!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "email")
-            {
-                let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-                
-                if (!regex.test(inputValue))
-                {
-                    errors.push({
-                        error: message || "Invalid email!",
-                        input: input
-                    });
-                }
-            }
-
-            else if (rule === "phone")
-            {
-                let err = false;
-
-                for (let index in ruleValue){
-                    let format = ruleValue[index];
-                    if (err)
-                        break;
-
-                    for (let i = 0; i < format.length; i++)
-                    {
-                        if (format[i] !== inputValue[i])
-                        {
-                            err = true;
-                            errors.push({
-                                error: message || "Values don't match!",
-                                input: input
-                            });
-                            break;
-                        }
-                    }
-                };
-            }
-
-            else 
-                throw new Error("Invalid rule: !\n\tInput: " + input.name + "\n\tRule: " + rule);
-        };
-
-        /**
-         * Goes through obj and rules and calls validate function
-         * @param {object} item - object with values (field, rule, etc)
-         */
-        const parseObj = function(item){
-            let rule = item.rule;
-            let input = item.input;
-
-            /**
-             * Since the input can take up to 3 shapes, we must validate to see which one we're handling and 
-             * act acording. If it's an array we've to go throught each one of it's elements, if it's an object
-             * we've to access the object elements, if it's neither we simply send the respective values
-             * @param {*} input - input we're preparing to validate 
-             */
-            const parseInput = function(input){
-                if (isArray(rule))
-                {
-                    Array.prototype.forEach.call(rule, function(current, index){
-                        validate(input, current.rule, current.message, current.value, current.optional);
-                    });
-                }
-                else if (isObject(rule))
-                    validate(input, rule.rule, rule.message, rule.value, rule.optional);
-                else
-                    validate(input, rule, item.message, item.value, item.optional);
-            }
-            
-            // since it's now possible validate more than one input with the same rules we need 
-            // check if we're hanlding a single input or several. If it's several we go throught 
-            // each one of them and call the respective function. If not we simply call the function
-            if (isArray(input))
-            {
-                Array.prototype.forEach.call(input, function(current){
-                    parseInput(current);
-                })
-            }
-            else 
-                parseInput(input);
-        }
-        
-        
-        // since it's now possible to send both an array with all objects to be validated or just 
-        // a single object to validate, we gotta check which one we're validating. If we're 
-        // validating several inputs, we go throught each of them invidually and validate them
-        // else we'll simply validate what we recieved
-        if (isArray(obj))
-        {
-            Array.prototype.forEach.call(obj, function(item){
-                parseObj(item);
-            });
-        }
-        else
-            parseObj(obj);
-
-        // It's also possible that we don't want to handle the errors here, and wanna 
-        // do it some other way, if so, we also gotta validate for that
-        if (handleErrors)
-        {
-            // if there are no errors remove any existing error displaying DOM and return true,
-            // indicating the inputs are valid
-            if (isEmpty(errors))
-            {
-                removeValdiationErrors();
-                return true;
-            }
-
-            // if not, we handle those DOM those errors and return false,
-            // indicating the inputs aren't valid
-            handleValidationErrors(errors);
-
-            return false;   
-        }
-        
-        return isEmpty(errors);
-    };
-
-    /**
-     * Creates DOM elements to show validation errors to the user
-     * @param {Object} errors - array with the errors: element that failed validation and error
-     */
-    const handleValidationErrors = function(errors){
-        removeValdiationErrors();
-
-        /**
-         * Adds class has-error to each element group (parent) for css purposes and 
-         * appends the error message to said parent.
-         * If there's more than one validation error for the same field, this prevents
-         * more than one error to be shown for that field at the same time
-         * @param {string} parent 
-         */
-        let addValidationErrors = function(parent, err){
-            if (parent.classList.contains("has-error")) 
-            return;
-            
-            let span = document.createElement("span");
-            parent.classList.add("has-error");
-            
-            span.innerHTML = err;
-            parent.appendChild(span);
-        }
-        
-        Array.prototype.forEach.call(errors, function(item, index){
-            let getParent = (elem) => {
-                return (util.isDOM(elem)) ? elem.parentElement : elem[0].parentElement;
-            }
-
-            if (isArray(item.input))
-            {
-                Array.prototype.forEach.call(item.input, function(input){
-                    addValidationErrors(getParent(input), item.error);
-                })
-            }
-            else 
-                addValidationErrors(getParent(item.input), item.error);
-        });
-    };
-
-    /**
-     * Removes error DOM elements by removing error-wrapper elements and 
-     * has-error class from parent elems
-     */
-    const removeValdiationErrors = function(){
-        let elems = document.getElementsByClassName("error-span");
-        
-        for (let i = elems.length-1; i >= 0; i--){
-            elems[i].parentElement.classList.remove("has-error");
-            elems[i].remove();
-        };
-    };
+    'use strict'
 
     /**
      * Validates value to check if it's number only
@@ -415,6 +100,9 @@
      * @return {boolean} - returns true if the value is object and false otherwise
      */
     const isFunction = (value) => {
+        if (isEmpty(value))
+            throw new Error("Value is empty!");
+
         return Object.toString.call(value) === '[object Function]' || typeof value === "function";
     };
 
@@ -461,7 +149,7 @@
      */
     Date.formatDate = function(date, type){
         if (util.isEmpty(date))
-            throw new Error("Invalid date: ", date);
+            throw new Error("Date is empty: ", date);
 
         try {
             let d = (date instanceof Date) ? date : new Date(date);
@@ -493,7 +181,6 @@
 
     // aggregades all functions in an objecto to export to the respective "platform"
     const utilities = {
-        isValid,
         isNumber,
         isEmpty,
         isPositive,
@@ -515,7 +202,6 @@
     // react
     else if (typeof navigator != 'undefined' && navigator.product == 'ReactNative'){
         module.exports = {
-            isValid,
             isNumber,
             isEmpty,
             isPositive,
